216fa8f531400b81de16d70ac461d5648e644075
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-06 09:27:57 +1000

    Added theme

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -30,11 +30,32 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const theme = {
+  fontStacks: {
+    sansSerif:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+  },
+  colors: {
+    neutralPalette: {
+      white: '#fff',
+      gray200: '#fafafa',
+      gray300: '#f5f5f5',
+      gray400: '#e8e8e8',
+      gray500: '#d9d9d9',
+      gray600: '#bfbfbf',
+      gray700: '#8c8c8c',
+      gray800: '#595959',
+      gray900: '#262626',
+      black: '#000'
+    }
+  }
+};
+
 function Page({ children }) {
   return (
     <>
       <GlobalStyle />
-      {children}
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
     </>
   );
 }
