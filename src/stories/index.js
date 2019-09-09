5a22821c07049b51c78b3bc40bd15cf65e9b5ae7
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-09 20:36:44 +1000

    Added stories for typography & colour

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,53 +5,87 @@ import { Title } from '../components/Typography';
 import Grid from '../components/Grid';
 import GridItem from '../components/GridItem';
 
-storiesOf('Fundamentals', module).add('Typography', () => (
-  <Page>
-    <Grid columns="100px 1fr 100px" height="100vh">
-      <GridItem columnStart="2">
-        <div
-          style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
-        >
-          <Title level={1} style={{ textAlign: 'center' }}>
-            H1 - Header
-          </Title>
-        </div>
-        <div
-          style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
-        >
-          <Title level={2} style={{ textAlign: 'center' }}>
-            H2 - Header
-          </Title>
-        </div>
-        <div
-          style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
-        >
-          <Title level={3} style={{ textAlign: 'center' }}>
-            H3 - Header
-          </Title>
-        </div>
-        <div
-          style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
-        >
-          <Title level={4} style={{ textAlign: 'center' }}>
-            H4 - Header
-          </Title>
-        </div>
-        <div
-          style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
-        >
-          <Title level={5} style={{ textAlign: 'center' }}>
-            H5 - Header
-          </Title>
-        </div>
-        <div
-          style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
-        >
-          <Title level={6} style={{ textAlign: 'center' }}>
-            H6 - Header
-          </Title>
-        </div>
-      </GridItem>
-    </Grid>
-  </Page>
-));
+storiesOf('Fundamentals', module)
+  .add('Typography', () => (
+    <Page>
+      <Grid columns="100px 1fr 100px" height="100vh">
+        <GridItem columnStart="2">
+          <div
+            style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
+          >
+            <Title level={1} style={{ textAlign: 'center' }}>
+              H1 - Header
+            </Title>
+          </div>
+          <div
+            style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
+          >
+            <Title level={2} style={{ textAlign: 'center' }}>
+              H2 - Header
+            </Title>
+          </div>
+          <div
+            style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
+          >
+            <Title level={3} style={{ textAlign: 'center' }}>
+              H3 - Header
+            </Title>
+          </div>
+          <div
+            style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
+          >
+            <Title level={4} style={{ textAlign: 'center' }}>
+              H4 - Header
+            </Title>
+          </div>
+          <div
+            style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
+          >
+            <Title level={5} style={{ textAlign: 'center' }}>
+              H5 - Header
+            </Title>
+          </div>
+          <div
+            style={{ marginTop: '1em', padding: '1em', background: '#efefef' }}
+          >
+            <Title level={6} style={{ textAlign: 'center' }}>
+              H6 - Header
+            </Title>
+          </div>
+        </GridItem>
+      </Grid>
+    </Page>
+  ))
+  .add('Color', () => (
+    <Page>
+      <Grid columns="100px 1fr 100px" height="100vh">
+        <GridItem columnStart="2">
+          <div
+            style={{ marginTop: '1em', padding: '1em', background: '#0facf3' }}
+          />
+          <div
+            style={{
+              marginTop: '1em',
+              padding: '1em',
+              background: '#3be8b0'
+            }}
+          />
+          <div
+            style={{
+              marginTop: '1em',
+              padding: '1em',
+              background: '#ffb900'
+            }}
+          />
+
+          <div
+            style={{
+              marginTop: '1em',
+              padding: '1em',
+              background: '#fc636b'
+            }}
+          />
+        </GridItem>
+      </Grid>
+    </Page>
+  ));
